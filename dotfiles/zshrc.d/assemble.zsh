2fba9d3bf538428e66f07472ad854425a89a17e3
David Tomaschik <david@systemoverlord.com>
2015-09-19 10:54:16 -0700

    Add ability to assemble shellcode.

---
+++ dotfiles/zshrc.d/assemble.zsh
@@ -0,0 +1,17 @@
+if [ -f "`command which nasm`" -a -f "`command which objdump`" ] ; then
+  assemble_shellcode() {
+    if [ -z "$1" ] ; then echo "Usage: $0 <assembly file>" >&2 ; return 1 ; fi
+    local NASM=`command which nasm`
+    local OBJDUMP=`command which objdump`
+    local TMPF=`mktemp`
+    local bytes
+    local byte
+    $NASM -f elf -o $TMPF $1
+    $OBJDUMP -M intel -d $TMPF | grep '^ ' | cut -f2 | while read -A bytes ; do
+      for byte in $bytes ; do
+        echo -n "\\\\x$byte"
+      done
+    done
+    echo
+  }
+fi
