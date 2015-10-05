1b0cec9ce23fa1c4ce409814318d22417e889c3c
David Tomaschik <david@systemoverlord.com>
2015-10-04 17:44:18 -0700

    Script to convert debian to kali.

---
+++ deb-to-kali.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ `whoami` != "root" ] ; then
+  echo "This must be run as root." >&2
+  exit 1
+fi
+
+cat >/etc/apt/sources.list.d/kali.list <<KALI_EOF
+deb http://http.kali.org/kali sana main non-free contrib
+deb-src http://http.kali.org/kali sana main non-free contrib
+deb http://security.kali.org/kali-security/ sana/updates main contrib non-free
+deb-src http://security.kali.org/kali-security/ sana/updates main contrib non-free
+KALI_EOF
+
+/usr/bin/apt-key add kali-repo.key
+/usr/bin/apt-get update
+/usr/bin/apt-get install kali-linux-full
