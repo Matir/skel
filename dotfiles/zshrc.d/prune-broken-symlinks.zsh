97d0f2f1d05bf51f65ca27c8f6f995c9db2c49f0
David Tomaschik <david@systemoverlord.com>
2015-11-12 10:33:25 -0800

    Prune broken symlinks now confirms.

--- dotfiles/zshrc.d/prune-broken-symlinks.zsh
+++ dotfiles/zshrc.d/prune-broken-symlinks.zsh
@@ -1,4 +1,26 @@
 prune-broken-symlinks() {
   setopt localoptions nounset
-  find $1 -type l -xtype l -print -delete
+  local ASK
+  local DIR
+  local FINDCMD
+  local i
+
+  if [[ "${1:-}" == "-y" ]] ; then
+    ASK=0
+    shift
+  else
+    ASK=1
+  fi
+  DIR=${1:-.}
+  FINDCMD=(find ${DIR} -type l -xtype l)
+  if (($ASK)) ; then
+    ${FINDCMD} -print
+    echo -n 'Delete these links? [y/n] '
+    if read -q ; then
+      ${FINDCMD} -delete
+    fi
+    echo
+  else
+      ${FINDCMD} -print -delete
+  fi
 }
