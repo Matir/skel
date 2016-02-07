dc64160e83026d74b09e6d971095021cb964df03
David Tomaschik <david@systemoverlord.com>
2016-02-07 13:07:53 -0800

    Move kali repo key into keys dir.

--- deb-to-kali.sh
+++ deb-to-kali.sh
@@ -11,6 +11,6 @@ cat >/etc/apt/sources.list.d/kali.list <<KALI_EOF
 deb http://http.kali.org/kali kali-rolling main contrib non-free
 KALI_EOF
 
-/usr/bin/apt-key add ${BASEDIR}/kali-repo.key
+/usr/bin/apt-key add ${BASEDIR}/keys/gpg/kali-repo.key
 /usr/bin/apt-get update
 /usr/bin/apt-get install -y kali-linux-full
