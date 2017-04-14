90185367321df86d130b2ebf0ef1fab05027c3b5
David Tomaschik <david@systemoverlord.com>
2017-04-13 19:26:42 -0700

    i3 updates.

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+LOCKER="i3lock -c 000000"
+/usr/bin/xss-lock -- "${LOCKER}" &
 exec /usr/bin/xautolock \
   -time 5 \
-  -locker "i3lock -c 000000" \
+  -detectsleep \
+  -locker "${LOCKER}" \
   -notify 30 \
   -notifier "notify-send -u critical -t 10000 -- 'LOCKING SCREEN IN 30 SECONDS'"
