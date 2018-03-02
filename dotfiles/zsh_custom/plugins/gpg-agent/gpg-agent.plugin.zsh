8c9627caa1c1bc8fda30579dc52efaf82d5a9f75
David Tomaschik <david@systemoverlord.com>
2018-03-01 21:08:56 -0800

    Chmod only if exists.

--- dotfiles/zsh_custom/plugins/gpg-agent/gpg-agent.plugin.zsh
+++ dotfiles/zsh_custom/plugins/gpg-agent/gpg-agent.plugin.zsh
@@ -4,13 +4,13 @@ local GPG_ENV=$HOME/.gnupg/gpg-agent.env
 
 function start_agent_nossh {
     eval $(/usr/bin/env gpg-agent --quiet --daemon --write-env-file ${GPG_ENV} 2> /dev/null)
-    chmod 600 ${GPG_ENV}
+    test -f ${GPG_ENV} && chmod 600 ${GPG_ENV}
     export GPG_AGENT_INFO
 }
 
 function start_agent_withssh {
     eval $(/usr/bin/env gpg-agent --quiet --daemon --enable-ssh-support --write-env-file ${GPG_ENV} 2> /dev/null)
-    chmod 600 ${GPG_ENV}
+    test -f ${GPG_ENV} && chmod 600 ${GPG_ENV}
     export GPG_AGENT_INFO
     export SSH_AUTH_SOCK
     export SSH_AGENT_PID
