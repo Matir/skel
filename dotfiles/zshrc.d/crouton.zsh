f96ad7773fb4fb89175555812f529d06a78a7bce
David Tomaschik <david@systemoverlord.com>
2019-04-29 18:06:14 -0700

    Fix xiwibg.

--- dotfiles/zshrc.d/crouton.zsh
+++ dotfiles/zshrc.d/crouton.zsh
@@ -2,6 +2,6 @@ if command -v xiwi >/dev/null 2>&1; then
   function xiwibg {
     local tmpf=$(mktemp)
     echo Logging to ${tmpf}
-    nohup xiwi "$@" >${tmpf} 2>&1 &
+    nohup xiwi "$@" >!${tmpf} 2>&1 &
   }
 fi
