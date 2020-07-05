244c5da0e765449280295ae82c92fb1452dfed27
David Tomaschik <david@systemoverlord.com>
2020-07-05 12:37:38 -0700

    Update clone script.

--- clone.sh
+++ clone.sh
@@ -4,15 +4,21 @@ set -ue
 
 # Script to clone and install
 
-if ! command -v git >/dev/null 2>&1 ; then
-  ( if [ "$EUID" != 0 ] ; then
-    sudo apt install -y git
-  else
-    apt install -y git
-    fi ) || ( echo 'Failed to install git!' >/dev/stderr; false)
-fi
+# Wrapped in a function to prevent incomplete execution if download is
+# interrupted
+function installer_main {
+  if ! command -v git >/dev/null 2>&1 ; then
+    ( if [ "$EUID" != 0 ] ; then
+      sudo apt install -y git
+    else
+      apt install -y git
+      fi ) || ( echo 'Failed to install git!' >/dev/stderr; false)
+  fi
 
-git clone https://github.com/Matir/skel.git ${HOME}/.skel
+  git clone https://github.com/Matir/skel.git ${HOME}/.skel
 
-${HOME}/.skel/install.sh
-${HOME}/.skel/install.sh packages minimal
+  ${HOME}/.skel/install.sh
+  ${HOME}/.skel/install.sh packages minimal
+}
+
+installer_main
