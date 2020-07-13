cc15b17e28ff4d03a90660569d9d78d0b5bb58c4
David Tomaschik <david@systemoverlord.com>
2020-07-12 18:04:46 -0700

    Tool to hold nvidia packages.

---
+++ bin/nvidia_hold.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function list_nvidia_installed {
+  dpkg-query -l '*nvidia*' | grep '^ii' | awk '{print $2}'
+}
+
+function hold_or_unhold {
+  apt-mark "${1:-hold}" $(list_nvidia_installed)
+}
+
+case "$1" in
+  hold|h)
+    hold_or_unhold hold
+    ;;
+  unhold|u)
+    hold_or_unhold unhold
+    ;;
+  *)
+    echo "$0 <hold|unhold>" >/dev/stderr
+    exit 1
+    ;;
+esac
