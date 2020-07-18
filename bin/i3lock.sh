9c147af8c1a1261aaf3bc89ca6eba62b53a582d3
David Tomaschik <david@systemoverlord.com>
2020-07-18 15:53:03 -0700

    Allow configuring locktime by environment variable.

--- bin/i3lock.sh
+++ bin/i3lock.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+LOCKTIME="${SCREENSAVER_MIN:-5}"
 LOCKER="i3lock -c 000000"
 # intentionally want word splitting below
 /usr/bin/xss-lock -- ${LOCKER} &
 exec /usr/bin/xautolock \
-  -time 5 \
+  -time "${LOCKTIME}" \
   -detectsleep \
   -locker "${LOCKER}" \
   -notify 30 \
