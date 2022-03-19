a49474fe6050e146e0d9d9c305e6e52d7b04461f
David Tomaschik <david@systemoverlord.com>
2022-03-19 15:23:50 -0700

    Add more tools

--- dotfiles/zshrc.d/usage.zsh
+++ dotfiles/zshrc.d/usage.zsh
@@ -1,3 +1,3 @@
-function duf {
+function duh {
      du -sk "$@" | sort -n | while read size fname; do for unit in K M G T P E Z Y; do if [ $size -lt 1024 ]; then echo -e "${size}${unit}\t${fname}"; break; fi; size=$((size/1024)); done; done
 }
