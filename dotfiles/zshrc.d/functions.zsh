3c8d8ca5e1ff80c78af743d07dfac373db7d2a25
David Tomaschik <david@systemoverlord.com>
2022-05-08 16:01:23 -0700

    Starship support.

--- dotfiles/zshrc.d/functions.zsh
+++ dotfiles/zshrc.d/functions.zsh
@@ -1,3 +1,9 @@
 function dumpenv {
   tr '\0' '\n' < /proc/${1}/environ
 }
+
+if test -x "${HOME}/tools/starship/starship" ; then
+  function starship_prompt {
+    eval $($HOME/tools/starship/starship init zsh)
+  }
+fi
