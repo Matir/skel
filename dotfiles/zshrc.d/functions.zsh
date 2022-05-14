dae254e24045633a7d35f778048a5052bb5923b1
David Tomaschik <david@systemoverlord.com>
2022-05-13 22:03:06 -0700

    Updates

--- dotfiles/zshrc.d/functions.zsh
+++ dotfiles/zshrc.d/functions.zsh
@@ -2,7 +2,11 @@ function dumpenv {
   tr '\0' '\n' < /proc/${1}/environ
 }
 
-if test -x "${HOME}/tools/starship/starship" ; then
+if test -x "/sbin/starship" ; then
+  function starship_prompt {
+    eval $(/sbin/starship init zsh)
+  }
+elif test -x "${HOME}/tools/starship/starship" ; then
   function starship_prompt {
     eval $($HOME/tools/starship/starship init zsh)
   }
