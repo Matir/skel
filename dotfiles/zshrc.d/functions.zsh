da43e7533d1dd6e2cf652570d3c2c4ef8d6c923c
David Tomaschik <david@systemoverlord.com>
2022-06-09 20:14:51 +0000

    Allow defaulting to starship

--- dotfiles/zshrc.d/functions.zsh
+++ dotfiles/zshrc.d/functions.zsh
@@ -3,11 +3,21 @@ function dumpenv {
 }
 
 if test -x "/sbin/starship" ; then
+  _STARSHIP_PATH="/sbin/starship"
   function starship_prompt {
     eval $(/sbin/starship init zsh)
   }
 elif test -x "${HOME}/tools/starship/starship" ; then
+  _STARSHIP_PATH="${HOME}/tools/starship/starship"
   function starship_prompt {
     eval $($HOME/tools/starship/starship init zsh)
   }
 fi
+if test -f ${HOME}/.zprompt ; then
+  if test "$(cat ${HOME}/.zprompt)" = "starship" ; then
+    if test -n "${_STARSHIP_PATH:-}" ; then
+      eval $(${_STARSHIP_PATH} init zsh)
+    fi
+  fi
+fi
+unset _STARSHIP_PATH
