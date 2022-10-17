373189f440ee1aadc77563e68ce9a0aa1adc94af
David Tomaschik <david@systemoverlord.com>
2022-10-16 20:47:00 -0700

    Add ffuf aliases

--- dotfiles/zshrc.d/aliases.zsh
+++ dotfiles/zshrc.d/aliases.zsh
@@ -6,3 +6,12 @@ if ! command -v bat >/dev/null 2>&1 ; then
     alias bat=$(command -v batcat)
   fi
 fi
+
+# FFUF aliases
+if command -v ffuf >/dev/null 2>&1 ; then
+  if test -d $HOME/tools/seclists ; then
+    alias ffuf-files="ffuf -c -w $HOME/tools/seclists/Discovery/Web-Content/raft-large-files.txt"
+    alias ffuf-dirs="ffuf -c -w $HOME/tools/seclists/Discovery/Web-Content/raft-large-directories.txt"
+    alias ffuf-quick="ffuf -c -w $HOME/tools/seclists/Discovery/Web-Content/quickhits.txt"
+  fi
+fi
