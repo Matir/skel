87797ca803fb82e5722a04509ff878f71caf8b17
David Tomaschik <david@systemoverlord.com>
2023-12-29 21:07:25 -0800

    Fix starship init

--- dotfiles/zshrc.d/functions.zsh
+++ dotfiles/zshrc.d/functions.zsh
@@ -5,18 +5,18 @@ function dumpenv {
 if test -x "/sbin/starship" ; then
   _STARSHIP_PATH="/sbin/starship"
   function starship_prompt {
-    eval $(/sbin/starship init zsh)
+    eval "$(/sbin/starship init zsh)"
   }
 elif test -x "${HOME}/tools/starship/starship" ; then
   _STARSHIP_PATH="${HOME}/tools/starship/starship"
   function starship_prompt {
-    eval $($HOME/tools/starship/starship init zsh)
+    eval "$($HOME/tools/starship/starship init zsh)"
   }
 fi
 if test -f ${HOME}/.zprompt ; then
   if test "$(cat ${HOME}/.zprompt)" = "starship" ; then
     if test -n "${_STARSHIP_PATH:-}" ; then
-      eval $(${_STARSHIP_PATH} init zsh)
+      eval "$(${_STARSHIP_PATH} init zsh)"
     fi
   fi
 fi
