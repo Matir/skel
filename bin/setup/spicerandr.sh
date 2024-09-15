c6fe0ff1c8667cb79bb9f4a592e4b5a746b9263d
David Tomaschik <david@systemoverlord.com>
2024-09-14 22:03:13 -0700

    Fix spicerandr

--- bin/setup/spicerandr.sh
+++ bin/setup/spicerandr.sh
@@ -8,9 +8,11 @@ PATH=/usr/bin:/bin:/usr/local/bin
 desktopuser=$(/bin/ps -ef  | /bin/grep -oP '^\w+ (?=.*vdagent( |$))') || exit 0
 export DISPLAY=:0
 export XAUTHORITY=$(eval echo "~$desktopuser")/.Xauthority
-/sbin/xrandr --output $(/sbin/xrandr | awk '/ connected/{print $1; exit; }') --auto
+/usr/bin/xrandr --output $(/usr/bin/xrandr | awk '/ connected/{print $1; exit; }') --auto
 EOF
+chmod 755 /usr/local/bin/x-resize
 
 cat >/etc/udev/rules.d/50-resize.rules <<"EOF"
 ACTION=="change",KERNEL=="card0", SUBSYSTEM=="drm", RUN+="/usr/local/bin/x-resize"
 EOF
+chmod 644 /etc/udev/rules.d/50-resize.rules
