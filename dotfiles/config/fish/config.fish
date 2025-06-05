ab2f62345fcfbb3bcb7b473e2d7aa607eb63196b
David Tomaschik <david@systemoverlord.com>
2025-06-05 15:43:14 -0700

    Update fish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,1 +1,11 @@
-fish_add_path --path {$HOME}/bin
+
+if test -x /opt/homebrew/bin/brew
+  /opt/homebrew/bin/brew shellenv fish | source
+end
+
+if command -q starship
+  source (starship init fish --print-full-init | psub)
+end
+
+# Want this at the bottom to put this path first
+fish_add_path --move --path {$HOME}/bin
