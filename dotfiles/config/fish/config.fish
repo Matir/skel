bbeb451251d47e4dc1315e147f905e21e646fd09
David Tomaschik <david@systemoverlord.com>
2025-06-10 08:57:25 -0700

    More completions

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,6 +1,13 @@
 
 if test -x /opt/homebrew/bin/brew
   /opt/homebrew/bin/brew shellenv fish | source
+  if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+  end
+
+  if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+  end
 end
 
 if command -q starship
