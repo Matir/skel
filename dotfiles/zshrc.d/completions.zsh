f3e82690f892f4bf1409d467670cb31d64f863c5
David Tomaschik <david@systemoverlord.com>
2025-06-12 17:23:35 -0700

    Update docker completion

--- dotfiles/zshrc.d/completions.zsh
+++ dotfiles/zshrc.d/completions.zsh
@@ -10,4 +10,8 @@
     missing_comp _rustup && rustup completions zsh > $COMPDIR/_rustup
     missing_comp _cargo && rustup completions zsh cargo > $COMPDIR/_cargo
   } || true
+
+  have_command docker && \
+    missing_comp _docker && \
+    docker completion zsh > $COMPDIR/_docker || true
 } &>/dev/null &!
